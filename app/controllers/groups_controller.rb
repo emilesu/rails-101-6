8d31cb55f2aaec1ef76e71bdf95a7f371f07730f
emilesu <emilesu@qq.com>
2017-03-04 13:01:33 +0800

    complement group#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,17 @@ class GroupsController < ApplicationController
       redirect_to groups_path
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+
+      redirect_to groups_path
+  end
+
 
 
   private
